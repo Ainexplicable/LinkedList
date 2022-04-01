07b35c23a04257cd15eb7569fbcf212ded46b7fd
Ainexplicable <silenthhh@outlook.com>
2022-04-01 11:49:37 -0500

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -1,4 +1,7 @@
 #pragma once
+#ifndef _LINKED_LIST_H
+#define _LINKED_LIST_H
+
 #include <limits.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -17,19 +20,37 @@ typedef struct LinkedList
 } LinkedList;
 
 LinkedList *create_linked_list();
+
 ChainNode *get_node(LinkedList *list, int index);
+
 _Bool is_element_index(LinkedList *list, int index);
+
 _Bool is_position_index(LinkedList *list, int index);
+
 void add(LinkedList *list, int index, int value);
+
 int remove(LinkedList *list, int index);
+
 int set(LinkedList *list, int index, int value);
+
 int get(LinkedList *list, int index);
+
 int index_of(LinkedList *list, int value);
+
 int last_index_of(LinkedList *list, int value);
+
 void clear(LinkedList *list);
+
 _Bool contains(LinkedList *list, int value);
+
 int front(LinkedList *list);
+
 int rear(LinkedList *list);
+
 void enqueue(LinkedList *list, int value);
+
 int dequeue(LinkedList *list);
+
 void print_string(LinkedList *list);
+
+#endif // _LINKED_LIST_H
